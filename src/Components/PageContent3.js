7f7176f1dc85f18a9662fe0e2a16eb85d1759099
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2023-12-14 10:21:54 +0200

    Product page

--- src/Components/PageContent3.js
+++ src/Components/PageContent3.js
@@ -1,16 +1,90 @@
-import React, {} from 'react';
+import React, { } from 'react';
+import Product from '../Images/Product.png';
 
 export default function PageContentA3() {
-  
-
     return (
-        <div class = "PageContentA3">
-            <div class = "Product"></div>
-            <div class = "Product"></div>
-            <div class = "Product"></div>
-            <div class = "Product"></div>
-            <div class = "Product"></div>
-            <div class = "Product"></div>
+        <div class="PageContentA3">
+            <div class="A3Title">
+                Products
+            </div>
+            <div class="Product">
+                <div class="ProductImageContainer">
+                    <img class="ProductImage" src={Product} />
+                </div>
+                <div class="ProductTextContainer">
+                    <div class="ProductText">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </div>
+                    <div class="ProductTextButton">
+                        Excepteur
+                    </div>
+                </div>
+            </div>
+            <div class="Product">
+                <div class="ProductImageContainer">
+                    <img class="ProductImage" src={Product} />
+                </div>
+                <div class="ProductTextContainer">
+                    <div class="ProductText">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </div>
+                    <div class="ProductTextButton">
+                        Excepteur
+                    </div>
+                </div>
+            </div>
+            <div class="Product">
+                <div class="ProductImageContainer">
+                    <img class="ProductImage" src={Product} />
+                </div>
+                <div class="ProductTextContainer">
+                    <div class="ProductText">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </div>
+                    <div class="ProductTextButton">
+                        Excepteur
+                    </div>
+                </div>
+            </div>
+            <div class="Product">
+                <div class="ProductImageContainer">
+                    <img class="ProductImage" src={Product} />
+                </div>
+                <div class="ProductTextContainer">
+                    <div class="ProductText">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </div>
+                    <div class="ProductTextButton">
+                        Excepteur
+                    </div>
+                </div>
+            </div>
+            <div class="Product">
+                <div class="ProductImageContainer">
+                    <img class="ProductImage" src={Product} />
+                </div>
+                <div class="ProductTextContainer">
+                    <div class="ProductText">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </div>
+                    <div class="ProductTextButton">
+                        Excepteur
+                    </div>
+                </div>
+            </div>
+            <div class="Product">
+                <div class="ProductImageContainer">
+                    <img class="ProductImage" src={Product} />
+                </div>
+                <div class="ProductTextContainer">
+                    <div class="ProductText">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </div>
+                    <div class="ProductTextButton">
+                        Excepteur
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
