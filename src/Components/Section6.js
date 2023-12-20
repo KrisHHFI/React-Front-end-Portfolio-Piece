6429fc461b99e538111da01038510260d68232cc
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2023-12-20 18:16:18 +0200

    Section 6 mobile accessible

--- src/Components/Section6.js
+++ src/Components/Section6.js
@@ -7,40 +7,38 @@ export default function Section6() {
             <div class="SectionTitle">
                 Contact
             </div>
-            <div className="PageContent6FormContainer">
-                <form class="PageContent6Form">
-                    <input
-                        type="text"
-                        placeholder="First Name*"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Last Name*"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Company Name*"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Title*"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Email*"
-                    />
-                    <textarea style={{ resize: 'none' }} type="text"
-                        placeholder="Message*"
-                    />
-                    <div className="PageContent6FormCheckBoxContainer">
-                        <input type="checkbox" id="TermsAndConditions" name="TermsAndConditions" />
-                        <label for="TermsAndConditions"> I agree to the terms and conditions.</label>
-                    </div>
-                    <div class="FormButton">
-                        Excepteur
-                    </div>
-                </form>
-            </div>
+            <form class="PageContent6Form">
+                <input
+                    type="text"
+                    placeholder="First Name*"
+                />
+                <input
+                    type="text"
+                    placeholder="Last Name*"
+                />
+                <input
+                    type="text"
+                    placeholder="Company Name*"
+                />
+                <input
+                    type="text"
+                    placeholder="Title*"
+                />
+                <input
+                    type="text"
+                    placeholder="Email*"
+                />
+                <textarea style={{ resize: 'none' }} type="text"
+                    placeholder="Message*"
+                />
+                <div className="PageContent6FormCheckBoxContainer">
+                    <input type="checkbox" id="TermsAndConditions" name="TermsAndConditions" />
+                    <label for="TermsAndConditions"> I agree to the terms and conditions.</label>
+                </div>
+                <div class="FormButton">
+                    Excepteur
+                </div>
+            </form>
         </div>
     );
 }
